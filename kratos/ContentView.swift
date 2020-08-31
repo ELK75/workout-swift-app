11a72504794a8cf20f7058f5fb5e99e5baf23cc1
ELK75 <elk75@pitt.edu>
2020-08-30 23:06:02 -0400

    Initial Commit

---
+++ kratos/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  kratos
+//
+//  Created by Elijah Kajinic on 8/30/20.
+//  Copyright © 2020 Elijah Kajinic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
